b58b6ae66676327a246e1b796ed9d1ea040655ad
Jhotika raja <140842575+Jojohere05@users.noreply.github.com>
2025-11-14 13:46:27 +0530

    Add root view path to URL patterns

--- equipment_backend/urls.py
+++ equipment_backend/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+     path('', root_view), 
 ]
 
 if settings.DEBUG:
